3de7965bf1fedae2e19e61745a461cd9dc55ac0c
LalitPro <140280rakesh@gmail.com>
2024-10-20 09:59:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+function addPara(p) {
+  p.innerHTML += "<p>Breaking Firewall...</p>";
+  p.innerHTML += "<p>Firewall Stage 1 breaking</p>";
+  p.innerHTML += "<p>Firewall Stage 2 breaking</p>";
+  p.innerHTML += "<p>Firewall Stage 3 breaking</p>";
+  p.innerHTML += "<p>Firewall Stage 4 breaking</p>";
+  p.innerHTML += "<p>Firewall Stage 5 breaking</p>";
+  p.innerHTML += "<p>Firewall Stage 6 breaking</p>";
+  p.innerHTML += "<p>Web page is getting information..........</p>";
+  p.innerHTML += "<p>Hacking password</p>";
+  p.innerHTML += '<p class="red">Invalid password</p>';
+  p.innerHTML += '<p class="red">Invalid password</p>';
+  p.innerHTML += '<p class="red">Invalid password</p>';
+  p.innerHTML += '<p class="red">Invalid password</p>';
+  p.innerHTML += '<p class="red">Invalid password</p>';
+  p.innerHTML += '<p class="red">Invalid password</p>';
+  p.innerHTML += '<p class="red">Invalid password</p>';
+  p.innerHTML += "<p>Finally password has been hacked</p>";
+  p.innerHTML +=
+    "<p>Removing Imprortant files from the system to hack ...................................................</p>";
+  p.innerHTML += "<p>Removing C://windows/win32/system/mainfile.imp</p>";
+  p.innerHTML += "<p>Removing C://windows/win32/system/windows.imp</p>";
+  p.innerHTML += "<p>Removing C://windows/win32/system/internet.imp</p>";
+  p.innerHTML += "<p>Removing C://windows/win32/system/microsoft.imp</p>";
+}
+function paraGroup() {
+  addPara(p);
+  addPara(p);
+  addPara(p);
+  addPara(p);
+  addPara(p);
+  addPara(p);
+  addPara(p);
+}
+function karoVinash() {
+  let btn = document.getElementById("button");
+  let h1 = document.getElementById("h1");
+  let body = document.getElementById("body");
+  let p = document.getElementById("p");
+
+  btn.remove();
+  h1.remove();
+
+  body.style.backgroundColor = "black";
+  body.style.alignItems = "flex-start";
+  body.style.justifyContent = "flex-start";
+  p.style.animationPlayState = "running";
+
+  paraGroup(p);
+  paraGroup(p);
+  paraGroup(p);
+  paraGroup(p);
+}
